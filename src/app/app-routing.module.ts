763da7fc9f871a7fb3cfc74735cd6edd5689bd06
Matheus Amorim <matheus_ads@hotmail.com>
2020-11-06 14:55:21 -0300

    add modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskListComponent } from './components/task-list/task-list.component'
+import { TaskDetailsComponent } from './components/task-details/task-details.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AddUserComponent } from './components/add-user/add-user.component'
+import { UserListComponent } from './components/user-list/user-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full'},
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks/:id', component: TaskDetailsComponent},
+  { path: 'add_task', component: AddTaskComponent },  
+  { path: 'users', component: UserListComponent },
+  { path: 'add_user', component: AddUserComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
